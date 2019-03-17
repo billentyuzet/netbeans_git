2f515816c4818fcbb858442d885bc26c95c63caa
netbeansZsoma <pzsferrari@freemail.hu>
2019-03-17 16:45:44 +0100

    kek lett a barack

--- src/kozos_projekt/Kozos_projekt.java
+++ src/kozos_projekt/Kozos_projekt.java
@@ -4,9 +4,9 @@ package kozos_projekt;
 public class Kozos_projekt 
 {
     public static void main(String[] args) 
-    {
+    {   
          System.out.println("Hello word"); 
-         System.out.println("Barack");
+         System.out.println(ConsoleColours.BLUE + "Barack");
          System.out.println("alma");
          System.out.println("szia linux!");
          System.out.println("szep estet!");
@@ -16,5 +16,6 @@ public class Kozos_projekt
          
          
     }
+    
     
 }
